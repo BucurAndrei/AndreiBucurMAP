f56a70cfca68c495bec077249afcc02fc22c3711
BucurAndrei <34206552+BucurAndrei@users.noreply.github.com>
2021-02-15 12:16:49 +0200

    just Made a class

--- src/main/main.java
+++ src/main/main.java
@@ -1,4 +1,11 @@
 package main;
 
-public class main {
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+class Main{
+
+public static void main(String[]args){
+    System.out.println("someline");
+        }
+
+
 }
